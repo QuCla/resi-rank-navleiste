e1a777697962feb8495843368c9d6f07b3a47da3
QuCla <122170839+QuCla@users.noreply.github.com>
2023-01-25 19:11:08 +0100

    include RAW url

---
+++ init.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Resi-Rank-Anzeige
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Current rank for rettungssimulator.online
+// @author       QuCla
+// @match        https://rettungssimulator.online/*
+// @icon         
+// @updateURL    https://github.com/QuCla/resi-rank-navleiste/raw/master/init.user.js
+// @downloadURL  https://github.com/QuCla/resi-rank-navleiste/raw/master/init.user.js
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+const script = document.createElement('script');
+
+//script.src = 'https://keineahnung.eu/resi-script-manager/js/bundle.js';
+script.src = 'https://github.com/QuCla/resi-rank-navleiste/raw/master/current-rank.js';
+document.head.appendChild(script);
