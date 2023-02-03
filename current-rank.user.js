a9381d8ddc48562f0d5fb919fa029d29c404ef9d
QuCla <122170839+QuCla@users.noreply.github.com>
2023-02-03 05:40:51 +0100

    removed failure due to periodic and createTextnode

--- current-rank.user.js
+++ current-rank.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Resi-Rang-Anzeige
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @version      1.3.2
 // @description  shows current rank for rettungssimulator.online
 // @author       QuCla
 // @match        https://rettungssimulator.online/*
@@ -24,11 +24,11 @@ function PlaceRank() {
         type : "GET",
         success : function(r) {
             let rank = r.toplistRank.toLocaleString();
-            let name = document.createTextNode('Dein Rang: ' + rank);
+            let name = ('Dein Rang: ' + rank);
             document.getElementsByClassName('muenzen_marken')[0].after(toplevel);
             toplevel.setAttribute('class', 'showRank');
             toplevel.appendChild(lowlevel);
-            lowlevel.appendChild(name);
+            lowlevel.innerHTML = name;
             lowlevel.setAttribute('data-tooltip', 'Dein aktueller Rang in der Topliste.');
             lowlevel.setAttribute('class', 'frame-opener');
             lowlevel.setAttribute('frame', '1/1/4/4');
